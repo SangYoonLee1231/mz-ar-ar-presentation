4d12d1de9a9737e4707fb03e07195c075af60738
SangYoonLee <prestro1231@hufs.ac.kr>
2023-09-14 20:41:04 +0900

    Add: 슬라이드 컴포넌트화 (배경색, 이미지 속성값으로 변경 가능)

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -4,29 +4,52 @@ import styled from "styled-components";
 const App = () => {
   return (
     <MainBody>
-      <Intro>
-        <Title>..</Title>
-        <SubTitle>..</SubTitle>
+      <Intro height="800px" bgColor="#ff6262">
+        <TitleArea>
+          <Title>..</Title>
+          <SubTitle>..</SubTitle>
+        </TitleArea>
         <ImageArea>..</ImageArea>
       </Intro>
-      <Page>page-two</Page>
-      <Page>page-three</Page>
-      <Page>page-four</Page>
-      <Page>page-five</Page>
-      <Outro>page-six</Outro>
+      <Page height="800px" bgColor="white">
+        page-one
+      </Page>
+      <Page height="800px" bgColor="white">
+        page-two
+      </Page>
+      <Page height="800px" bgColor="white">
+        page-three
+      </Page>
+      <Page height="800px" bgColor="white">
+        page-four
+      </Page>
+      <Page height="800px" bgColor="white">
+        page-five
+      </Page>
+      <Outro height="800px" bgColor="white">
+        page-six
+      </Outro>
     </MainBody>
   );
 };
 
 const MainBody = styled.div``;
 
+//
+
 const Intro = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  height: 800px;
-  background-color: #ff6262;
+  height: ${(props) => props.height || "800px"};
+  background-color: ${(props) => props.bgColor || "white"};
+`;
+
+const TitleArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
 const Title = styled.div``;
@@ -35,8 +58,18 @@ const SubTitle = styled.div``;
 
 const ImageArea = styled.div``;
 
-const Page = styled.div``;
+//
 
-const Outro = styled.div``;
+const Page = styled.div`
+  height: ${(props) => props.height || "800px"};
+  background-color: ${(props) => props.bgColor || "white"};
+`;
+
+//
+
+const Outro = styled.div`
+  height: ${(props) => props.height || "800px"};
+  background-color: ${(props) => props.bgColor || "white"};
+`;
 
 export default App;
